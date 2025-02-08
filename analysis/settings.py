ebd96392ca7807db883f30c0162e5caab3b92c8d
Adeleye-Adeola <mightydeveloper500@gmail.com>
2025-02-08 16:25:40 +0100

    Setup for render deployment

--- analysis/settings.py
+++ analysis/settings.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -71,6 +72,12 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+STATIC_URL = '/static/'
+
+# Where the static files will be collected
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 
 
 # Password validation
